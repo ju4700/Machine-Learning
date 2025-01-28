6226738d4b737568f59863239fb9172a367c381f
ju4700 <jalal.dev.design@gmail.com>
2025-01-29 00:55:45 +0600

    Neural Network From Scratch

--- main-o1.py
+++ main-o1.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+def predict(dark_clouds, temperature_drop):
+    storm = 0
+    # if dark_clouds == 1 and temperature_drop == 1:
+    #     storm = 1
+    #
+    return storm
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# print(predict(0, 1))
+# print(predict(1, 1))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import random
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Neuron:
+    def __init__(self):
+        self.w1 = random.random()
+        self.w2 = random.random()
+        self.t = random.random()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def predict(self, dark_clouds, temperature_drop):
+        storm = 0
+        if (dark_clouds * self.w1 + temperature_drop * self.w2) > self.t:
+            storm = 1
+        return storm
+
+    def learn(self, dark_clouds, temperature_drop, storm):
+        error = self.predict(dark_clouds, temperature_drop) - storm
+        self.w1 = error * dark_clouds / 100
+        self.w2 = error * temperature_drop / 100
+        self.t += error / 100
+
+data = [[1, 1, 1],
+        [1, 0, 1],
+        [0, 1, 1],
+        [0, 0, 1]]
+
+def runTraining(neuron):
+    for row in data:
+        neuron.learn(row[0], row[1], row[2])
+
+def runTesting(neuron):
+    return [neuron.predict(row[0], row[1]) for row in data]
+
+neuron = Neuron()
+
+while True:
+    output = runTesting(neuron)
+    print("Predictions:", output)
+    if output == [row[2] for row in data]:
+        print("Training complete!")
+        break
+
+    runTraining(neuron)
